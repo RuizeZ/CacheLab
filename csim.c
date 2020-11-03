64c3c713836214b3cf400f690c7a0742cdef50c6
RuizeZ <imruize@126.com>
2020-11-03 16:14:56 +0800

    Identical with example

--- csim.c
+++ csim.c
@@ -3,12 +3,19 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#define SIZE 100
 
 void readargv(int argc, char* argv[]);
+void enQueue(unsigned *setaddr);
+unsigned* deQueue();
 void simulate();
 
 int HelpFlag, VerboseFlag, SetIndexBits, LinesPerSet, BlockBits;
+int hit = 0, miss = 0, evictions = 0;
 char filename[50];
+unsigned *items[SIZE];
+int front = -1, rear = -1;
 FILE *pFile;
 
 void readargv(int argc, char* argv[])
@@ -46,6 +53,10 @@ void simulate(){
     char identifier;
     unsigned address;	
     int	size;	
+    unsigned setindex, tag;
+    unsigned *setaddr, *pi;
+    unsigned linesize = pow(2, BlockBits)+2;
+
     if (HelpFlag == 1)
     {
         printf("Usage: ./csim-ref [-hv] -s <num> -E <num> -b <num> -t <file>\n");
@@ -64,15 +75,66 @@ void simulate(){
     }
     else
     {
+        pi = malloc(pow(2, SetIndexBits) * LinesPerSet * linesize * sizeof(int));
+        //printf("pi = %p\n", pi);
         while(fscanf(pFile, " %c %x,%d", &identifier, &address, &size)>0)	
         {	
+            
             if(identifier == ' ')
             {
                 continue;
             }
             else
             {
+                // Separate address into differnt part in cache
+                // blockoffset = (address << (32 - BlockBits)) >> (32 - BlockBits);
+                setindex = (address << (32 - BlockBits - SetIndexBits)) >> (32 - SetIndexBits);
+                tag = address >> (BlockBits + SetIndexBits);
+                // printf("blockoffset = %d\n", blockoffset);
+                // printf("setindex = %d\n", setindex);
+                // printf("tag = %d\n", tag);
+
+                //find the set
+                setaddr = pi + (setindex * linesize);
+                // printf("setaddr %p\n", setaddr);
+                enQueue(setaddr);
+                //if miss
+                identifier_M:
+                // printf("tag = %d\n", tag);
+                // printf("*(setaddr + 1) = %d\n", *(setaddr + 1));
+                if ((tag != *(setaddr + 1)) || (!*setaddr) )
+                {
+                    //printf("miss\n");
+                    miss++;
+                    setaddr = deQueue();
+                    //printf("current setaddr %p\n", setaddr);
+                    *setaddr = 1;
+                    *(setaddr + 1) = tag;
+                    if(identifier == 'M'){
+                        goto identifier_M;
+                    }
+                }
+                else
+                {
+                    //printf("hit\n");
+                    hit++;
+                }
                 
+                // //if data load
+                // if(identifier == 'L')
+                // {
+                    
+                // //if miss
+
+                    
+                // }
+                // else if(identifier == 'M')
+                // {
+
+                // }
+                // else if(identifier == 'S'){
+
+                // }
             }
             
         }
@@ -83,11 +145,47 @@ void simulate(){
 
     
 }
+void enQueue(unsigned * setaddr) {
+    if (rear == SIZE - 1)
+    printf("Queue is Full!!\n");
+    else {
+        if (front == -1)
+            front = 0;
+        rear++;
+        items[rear] = setaddr;
+        // printf("Inserted -> %p\n", items[rear]);
+    }
+}
+
+unsigned* deQueue() {
+    unsigned * currentsetaddr;
+    if (front == -1)
+        printf("Queue is Empty!!\n");
+    else {
+        currentsetaddr = items[front];
+        // printf("Deleted : %p\n", items[front]);
+        front++;
+        if (rear != 0)
+        {
+            //printf("eviction\n");
+            evictions++;
+        }
+        
+        if (front > rear)
+            front = rear = -1;
+    }
+    return currentsetaddr;
+}
+
+
+
+
+
 
 int main(int argc, char* argv[])
 {
     readargv(argc, argv);
     simulate();
-    // printSummary(0, 0, 0);
+    printSummary(hit, miss, evictions);
     return 0;
 }
